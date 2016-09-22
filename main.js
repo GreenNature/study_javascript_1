f93f6b2a6c02341dcb1f3d92f17cdadcd1008586
GreenNature <nobu1015@naver.com>
2016-09-22 15:26:32 +0900

    I update my js file

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+var express = require('express');
+var HOST = '192.168.20.2';
+var PORT = '3000';
+var app = express();
 
+app.get('/',function(req, res){
+  res.send('Hello World!');
+})
+
+app.listen(PORT,HOST,function () {
+  console.log('Server On!');
+});
